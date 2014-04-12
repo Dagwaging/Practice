12a5a15cce9a9b9ffe46f481e46c038ca36b12be
Dagwaging <dagwaging@gmail.com>
2014-04-12 03:27:26 -0400

    Added simple Pebble test notification

--- Practice/src/com/hairforce/grouponalert/MainActivity.java
+++ Practice/src/com/hairforce/grouponalert/MainActivity.java
@@ -1,15 +1,46 @@
 package com.hairforce.grouponalert;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
+		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		sendAlertToPebble();
 	}
 
+	private void sendAlertToPebble() {
+		final Intent intent = new Intent("com.getpebble.action.SEND_NOTIFICATION");
+		
+		final Map<String, String> data = new HashMap<String, String>();
+		
+		data.put("title", "This was a triumph");
+		data.put("body", "I'm making a note here, huge success");
+		
+		final JSONObject jsonData = new JSONObject(data);
+		final String notificationData = new JSONArray().put(jsonData).toString();
+		
+		intent.putExtra("messageType", "PEBBLE_ALERT");
+		intent.putExtra("sender", "Groupon Alert");
+		intent.putExtra("notificationData", notificationData);
+		
+		sendBroadcast(intent);
+	}
 }
