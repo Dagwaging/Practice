18ab070b268d7c713f4c6426beab2ec992a289d7
Dagwaging <dagwaging@gmail.com>
2014-04-12 08:36:20 -0400

    Added location and activity updates and buttons

--- Practice/src/com/hairforce/grouponalert/MainActivity.java
+++ Practice/src/com/hairforce/grouponalert/MainActivity.java
@@ -6,24 +6,41 @@ import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity implements OnClickListener {
+	private Button start;
+	private Button stop;
+	private ActivityUpdater activityUpdater;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);
+		
+		start = (Button) findViewById(R.id.button1);
+		stop = (Button) findViewById(R.id.button2);
+		
+		start.setOnClickListener(this);
+		stop.setOnClickListener(this);
+		
+		activityUpdater = new ActivityUpdater(this, ActivityService.class);
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		
-		sendAlertToPebble();
+		//sendAlertToPebble();
+		
+		//openInGrouponApp();
 	}
 
 	private void sendAlertToPebble() {
@@ -42,5 +59,26 @@ public class MainActivity extends Activity {
 		intent.putExtra("notificationData", notificationData);
 		
 		sendBroadcast(intent);
+	}
+	
+	private void openInGrouponApp() {
+		final Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse("http://www.groupon.com/redirect/deals/seadog-cruises-13"));
+		
+		startActivity(intent);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.button1:
+			activityUpdater.start(2000);
+			
+			break;
+		case R.id.button2:
+			activityUpdater.stop();
+			
+			break;
+		}
 	}
 }
